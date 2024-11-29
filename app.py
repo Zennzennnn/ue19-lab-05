a6ed025c524872c6a722f336539096410e0012eb
Zennzennnn <zl.truong@student.helmo.be>
2024-11-29 20:42:11 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import requests
+
+def joke():
+    api = "https://official-joke-api.appspot.com/random_joke"
+    response = requests.get(api).json()
+    return f"{response["setup"]}\n{response['punchline']}"
+
+print(joke())
